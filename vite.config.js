38b38170c9200b8532e03a360d5d9cb7c3daae36
Peter-sour <malakulkabirarrabbani@gmail.com>
2026-01-09 09:33:50 +0700

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/malayeka-hijab/', // Sesuaikan dengan nama repo di GitHub
   plugins: [react()],
+  base: '/' // <--- PENTING UNTUK DEPLOY KE GITHUB PAGES
 })
